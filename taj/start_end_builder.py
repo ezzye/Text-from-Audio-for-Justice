b38b41f12b672e33e5a6a0e6060de61c9ef53277
ezzye <ezzy.elliott@gmail.com>
2019-12-03 09:35:25 +0000

    Add test and code for making chunk phrase

--- taj/start_end_builder.py
+++ taj/start_end_builder.py
@@ -1,3 +1,25 @@
+import re
+
+
 class ChunkPhraseBuilder(object):
     def compile_chunk_phrases(self, transcription, markedup_taj_file):
-        return markedup_taj_file
+        chunk_list = markedup_taj_file.split('|')
+        chunk_word_list = []
+        words = transcription['words']
+        print(words)
+        word_count_end = 0
+        chunk_phrase = []
+        for chunk in chunk_list:
+            chunk_words = re.sub("[^\w]", " ",  chunk).split()
+            chunk_word_list.append(chunk_words)
+        print(chunk_word_list)
+        for index, chunk_word in enumerate(chunk_word_list):
+            word_count_start = word_count_end
+            word_count_end = word_count_start + len(chunk_word)
+            start = words[word_count_start]['start']
+            end = words[word_count_end-1]['end']
+            chunk_phrase.append({'name': f'chunk{index + 1}', 'start': start, 'end': end})
+        return chunk_phrase
+
+    def make_markup_file(self, punct_text):
+        return re.sub(r"\.", ".|", punct_text)
--- tests/test_start_end_builder.py
+++ tests/test_start_end_builder.py
@@ -1,17 +1,25 @@
 import unittest
 
 from start_end_builder import ChunkPhraseBuilder
-from tests import load_fixture, load__document
+from utils import load_fixture, load__document
 
 
 class TestChunkPhraseBuilder(unittest.TestCase):
-    def test_compile_chunk_phrase(self):
+    def test_make_markedup(self):
         builder = ChunkPhraseBuilder()
-        transcription = load_fixture('kaldi_small_doc')
-        markedup_taj_file = load__document('markedup.taj')
+        transcription = load_fixture('kaldi_small_doc.json')
+        expected = "Oh.| And A A over"
+        actual = builder.make_markup_file(transcription["punct"])
+        self.assertEqual(actual, expected)
+
+
+def test_compile_chunk_phrase(self):
+        builder = ChunkPhraseBuilder()
+        transcription = load_fixture('kaldi_small_doc.json')
+        markedup_taj_file = builder.make_markup_file(transcription["punct"])
         expected = [
-            {'name': 'chunk1', 'start': 0.0, 'end': 1.64},
-            {'name': 'chunk2', 'start': 1.9, 'end': 21.7}
+            {'name': 'chunk1', 'start': 0.0, 'end': 1.13},
+            {'name': 'chunk2', 'start': 1.13, 'end': 21.7}
         ]
         actual = builder.compile_chunk_phrases(transcription, markedup_taj_file)
         self.assertEqual(actual, expected)
